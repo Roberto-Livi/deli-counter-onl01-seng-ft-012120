2376d872e83ac0f5036128b59fef118f78d4f8a1
Roberto Livi <robertomlivi@outlook.com>
2020-01-23 17:34:42 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -28,3 +28,13 @@ def now_serving(katz_deli)
     katz_deli.shift
   end
 end
+
+line(katz_deli)
+now_serving(katz_deli)
+take_a_number(katz_deli, "Ada")
+line(katz_deli)
+take_a_number(katz_deli, "Grace")
+take_a_number(katz_deli, "Spencer")
+line(katz_deli)
+now_serving(katz_deli)
+line(katz_deli)
